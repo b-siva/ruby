5719f961ef5b57df454825e4dc44a9a2696c94cf
Sivaprakasam Boopathy <sb970g@yp.com>
2013-08-22 15:12:29 -0700

    recursive printing

--- print_star.rb
+++ print_star.rb
@@ -6,3 +6,14 @@ while(i != 0) do
   forward = false if i == n
   forward ? i += 1 : i -= 1  
 end
+
+puts 'recursive printing'
+puts '------------------'
+def print_star(i,n,forward)
+  return if i == 0
+  puts('*' * i)
+  forward = false if i == n
+  forward ? print_star(i+=1,n,forward) : print_star(i-=1,n,forward)
+end
+
+print_star(1,5,true)
--- print_star_backward.rb
+++ print_star_backward.rb
@@ -6,3 +6,12 @@ while(n >= i) do
   n-=1
 end
 
+puts 'recursive printing'
+puts '------------------'
+def print_star(i,n)
+  return if i > n 
+  puts('*' * n)
+  print_star(i,n-=1)
+end
+
+print_star(1,5)
