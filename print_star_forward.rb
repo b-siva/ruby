31bb6c1f48bdc854769a9f62e2d5a123baa99780
Sivaprakasam Boopathy <sb970g@yp.com>
2013-08-22 14:50:33 -0700

    added recursive printing

--- print_star_forward.rb
+++ print_star_forward.rb
@@ -5,3 +5,13 @@ while(i <= n) do
   i+=1
 end
 
+puts 'recursive printing'
+puts '------------------'
+
+def print_star(i,n)
+  return if i > n
+  puts('*' * i)
+  print_star(i+=1,n)
+end
+
+print_star(1,5)
